932ff478302970dfc9fb29e06aa230e8c6c3750d
Gustavo Alfredo Marín Sáez <aterbonus@gmail.com>
2022-11-08 00:45:33 -0300

    fix: critters preload

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@ export default defineNuxtConfig({
 	modules: ['@nuxtjs/critters', '@unocss/nuxt', '@vueuse/nuxt'],
 	unocss: {
 		preflight: true
+	},
+	critters: {
+		config: {
+			preload: false
+		}
 	}
 })
