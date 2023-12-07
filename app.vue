1e1490ec72d0192bedbcdbfc9f9fd485b720504b
Gustavo Alfredo Marín Sáez <aterbonus@gmail.com>
2023-12-07 00:50:43 -0300

    fix: use svh if possible

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="grid min-h-screen place-items-center">
-		<h1 class="text-8 font-bold">Aterbonu's Nuxt Template</h1>
+	<div class="grid min-h-screen supports-[(min-height:100svh)]:min-h-100svh place-items-center">
+		<h1 class="text-8 font-bold text-center">Aterbonus' Nuxt Template</h1>
 	</div>
 </template>
