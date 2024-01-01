ac6085b3cc675e3c425ccc00b5c41cd76d09c84f
Gustavo Alfredo Marín Sáez <aterbonus@gmail.com>
2024-01-01 03:13:12 -0300

    fix: temporary fix to unocss issue with new nuxt version + innecesary critters
    https://github.com/unocss/unocss/issues/3472
    when the issue is solve we must remove the inlineStyles: false on nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,9 @@
 export default defineNuxtConfig({
-	modules: ['@nuxtjs/critters', '@unocss/nuxt', '@vueuse/nuxt'],
+	modules: ['@unocss/nuxt', '@vueuse/nuxt'],
 	unocss: {
 		preflight: true
 	},
-	critters: {
-		config: {
-			// @ts-expect-error false exists as an option, but the typing is wrong
-			preload: false
-		}
+	features: {
+		inlineStyles: false
 	}
 })
