5a28578e2efd8f24268fba5113e0223ff318a049
Gustavo Alfredo Marín Sáez <aterbonus@gmail.com>
2026-07-19 02:39:39 -0400

    fix: use typescript 6 until eslint supports typescript 7

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 export default defineNuxtConfig({
 	compatibilityDate: '2025-07-15',
+	future: {
+		compatibilityVersion: 5
+	},
 	devtools: { enabled: true },
 	modules: ['@unocss/nuxt', '@vueuse/nuxt'],
 	unocss: {
