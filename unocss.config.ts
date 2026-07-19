915de99ed8da204b3002a4cf390796fda712b610
Gustavo Alfredo Marín Sáez <aterbonus@gmail.com>
2026-07-19 04:43:00 -0400

    feat: use presetWind4

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig, presetIcons, presetUno, presetWebFonts, transformerVariantGroup } from 'unocss'
+import { defineConfig, presetIcons, presetWebFonts, presetWind4, transformerVariantGroup } from 'unocss'
 
 export default defineConfig({
 	presets: [
-		presetUno(),
+		presetWind4(),
 		presetIcons(),
 		presetWebFonts({
 			fonts: {
